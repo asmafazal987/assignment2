6d8fcaa0dd2e348b1c8dd694453c7698abbd5da6
asmafazal987 <asmafazal987@gmail.com>
2023-08-25 21:20:29 +0500

    first assignment

---
+++ Assignment2_Module.py
@@ -0,0 +1,120 @@
+class Assignment2_Module:
+
+
+    # Question no 1
+    def list_to_dict(self):
+        li1 = [int(x) for x in input("Enter your list of integers separated by spaces: ").split()]
+        li2 = input("Enter your list of strings separated by spaces: ").split()
+
+        d = {}
+        for i in range(len(li1)):
+            d[li1[i]] = li2[i]
+        print(d)
+
+    # Question no 2
+    def replace_dictionary(self):
+        input_string = input("Enter a dictionary in the format {key1:value1, key2:value2, ...}: ")
+        input_dict = eval(input_string) 
+        for key,value in input_dict.items():
+            if input_dict[key] == 'D':
+                input_dict[key] = 'apple'
+        return input_dict
+
+    
+    # Question no 3
+    def update_dictionary(self):
+        input_string = input("Enter a dictionary in the format {'key1': value1, 'key2': value2, ...}: ")
+        input_dict = eval(input_string)
+        for key,value in input_dict.items():
+            if input_dict[key] == 7:
+                input_dict[key] = 10
+        return input_dict
+  
+
+    # Question no 4
+    def find_common_elements(self):
+        input_string1 = input("Enter the first list as comma-separated values: ")
+        list1 = [int(x) for x in input_string1.split(',')]
+
+        input_string2 = input("Enter the second list as comma-separated values: ")
+        list2 = [int(x) for x in input_string2.split(',')]
+        for item1 in list1:
+            for item2 in list2:
+                if item1 == item2:
+                    print(item1, end=' ,')
+
+    # Question no 5
+    def use_of_continue(self):
+        start = int(input("Enter the starting number: "))
+        end = int(input("Enter the ending number: "))
+        for i in range(start, end+1):
+            if i % 2 == 0:
+                continue
+            else:
+                print(i)
+    
+    # Question no 6
+    def update_list_element(self):
+        input_string = input("Enter a list containing different data types: ")
+        input_list = eval(input_string) 
+
+        index = int(input("Enter the index of the dictionary element in the list: "))
+        key = int(input("Enter the key for the dictionary element: "))
+        value = input("Enter the value to update with: ")
+        
+        input_list[index][key] = value
+
+        print(input_list)
+
+    # Question no 7
+    def add_occupation_to_nested_dict(self):
+        input_string = input("Enter a nested dictionary in the format {'person1': {'age': age1, 'city': 'city1'}, 'person2': {'age': age2, 'city': 'city2'}, ...}: ")
+        input_dict = eval(input_string) 
+        for person, details in input_dict.items():
+            print("hi",details)
+            details['occupation'] = 'Engineer'
+            
+            return input_dict
+            
+    # Question no 8
+    def filter_age_above_18(self):
+        input_string = input("Enter a dictionary of names and ages in the format {'name1': age1, 'name2': age2, ...}: ")
+        input_dict = eval(input_string) 
+        new_age_dict = {}
+        for name, age in input_dict.items():
+            if age >= 18:
+                new_age_dict[name] = age
+        return new_age_dict
+
+    # Question no 9
+    def sort_num_list(self):
+        input_list = [int(x) for x in input("Enter your list of integers separated by spaces: ").split()]
+        for i in range(len(input_list)):
+            for j in range(i+1, len(input_list)):
+                if input_list[i] > input_list[j]:
+                    input_list[i], input_list[j] = input_list[j], input_list[i]
+        return input_list
+    
+     # Question no 10
+    def sort_string_list(self):
+        input_list = input('Enter your list of Alphabets or words seperated by spaces').split()
+        for i in range(len(input_list)):
+            for j in range(i+1, len(input_list)):
+                if input_list[i] > input_list[j]:
+                    input_list[i], input_list[j] = input_list[j], input_list[i]
+        return input_list
+    
+    # Question no 11
+    def sort_alphanumeric_list(self):
+        input_list = input('Enter your list of characters and integers seperated by spaces').split()
+        for i in range(len(input_list)):
+            for j in range(i+1, len(input_list)):
+                if input_list[i] > input_list[j]:
+                    input_list[i], input_list[j] = input_list[j], input_list[i]
+        return input_list
+    
+    
+    
+
+
+    
---
+++ main.py
@@ -0,0 +1,71 @@
+from Assignment2_Module import Assignment2_Module
+
+def main():
+    obj = Assignment2_Module()
+
+    while True:
+
+        print('Enter your question num')
+        print('Enter 1 to convert lists to dictionary')
+        print('Enter 2 to replace the specific values of dictionary')
+        print('Enter 3 to update the specific values of dictionary')
+        print('Enter 4 to find the common elements in the ;lists')
+        print('Enter 5 to check the use of continue in python')
+        print("Enter 6 to replace the dictionary's value in a list")
+        print('Enter 7 to add new key value pair in nested dictionary')
+        print('Enter 8 to filter out the persons having age above or equal to 18')
+        print('Enter 9 to sort a list numbers in asscending order ')
+        print('Enter 10 to sort a list of alphabets or strings in asscending order ')
+        print('Enter 11 to sort a list of strings and integers in asscending order ')
+        print('Enter 12 to Exit')
+        
+        ques = int(input('Enter the question number (1 to 12): '))
+        if ques == 1 :
+            result = obj.list_to_dict()
+            print(result)
+        elif ques == 2 :
+            result = obj.replace_dictionary()
+            print(result)
+        elif ques == 3 :
+            result = obj.update_dictionary()
+            print(result)
+        elif ques == 4 :
+            result = obj.find_common_elements()
+            print(result)
+        elif ques == 5 :
+            result = obj.use_of_continue()
+            print(result)
+        elif ques == 6 :
+            result = obj.update_list_element()
+            print(result)
+        elif ques == 7 :
+            result = obj.add_occupation_to_nested_dict()
+            print(result)
+        elif ques == 8 :
+            result = obj.filter_age_above_18()
+            print(result)
+        elif ques == 9:
+            result = obj.sort_num_list()
+            print(result)
+        elif ques == 10:
+            result = obj.sort_string_list()
+            print(result)
+        elif ques == 11:
+            result = obj.sort_alphanumeric_list()
+            print(result)
+        elif ques == 12:
+            break
+        else:
+            print("Invalid question number.")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+   
